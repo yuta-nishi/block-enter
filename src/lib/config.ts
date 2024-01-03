bfb3024c6ce4501ce3a0281b16c7d8a536b3f705
yuta-nishi <ypmtk437.0209@gmail.com>
2024-01-04 02:43:57 +0900

    fix: add bard url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,3 @@
-export const urls = ['https://chat.openai.com/*', 'https://bard.google.com/*'];
+// content_scripts's config must be hard-coded.
+// export const urls = ['https://chat.openai.com/*', 'https://bard.google.com/*'];
 export const hostnames = ['chat.openai.com', 'bard.google.com'];
