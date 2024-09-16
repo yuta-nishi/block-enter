102c2d86e4236156904d16ece39376b0cf54ec7c
yuta-nishi <ypmtk437.0209@gmail.com>
2024-09-16 22:41:45 +0900

    chore: Minor correction

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    include: ['test/**/*.test.{ts,tsx}'],
+    include: ['tests/**/*.test.{ts,tsx}'],
     globals: true,
     environment: 'happy-dom',
     setupFiles: './vitest.setup.ts',
